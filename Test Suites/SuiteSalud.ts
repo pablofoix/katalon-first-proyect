84063f6082dc7b6de789265771eea9ec17cc2b7b
Pablo Foix <pabloffoix@gmail.com>
2022-10-27 18:58:06 -0300

    first commit

---
+++ Test Suites/SuiteSalud.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuiteSalud</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>88f92778-deb6-4e93-8106-4d78b5928752</testSuiteGuid>
+   <testCaseLink>
+      <guid>b1b41327-e580-4618-a217-d93a4f2d3753</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SuiteSalud/TASAS</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>247f000b-8022-4cce-9aef-a9de937cb885</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SuiteSalud/ReservarTurno</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>3a71de81-40ef-4104-bc3a-6f0e5a54853e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SuiteSalud/Untitled Test Case</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>e724f49e-5c42-4e54-a544-d5ad7d1d4a8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SuiteSalud/CargarPagina</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
